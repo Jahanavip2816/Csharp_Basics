c7aedfa7d4a450577c41c6b283ca44b1e221e3ba
Jahanavip2816 <jahanavi.p@saksoft.com>
2025-09-16 17:03:41 +0530

    Added Gitignore

--- obj/Debug/net9.0/CrimeApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/CrimeApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CrimeApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fbfde564544ea10711ae28115cf14abe235d46c3")]
 [assembly: System.Reflection.AssemblyProductAttribute("CrimeApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CrimeApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
